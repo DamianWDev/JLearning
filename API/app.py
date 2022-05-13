bc58f9d969f7c20d8a0dae56e9edda488c21be8b
Dawid Kiełkowski <dawid55a@gmail.com>
2022-05-14 00:30:21 +0200

    added manga image upload endpoint

--- API/app.py
+++ API/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify, request, flash, redirect, url_for
+from flask import Flask, jsonify, request, flash, redirect, url_for, Response, send_file
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from flask_sqlalchemy import SQLAlchemy
@@ -7,7 +7,7 @@ from werkzeug.utils import secure_filename
 
 from Models.models import *
 
-UPLOAD_FOLDER = '/'
+UPLOAD_FOLDER = '/app/images'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 DB_STRING = r'postgresql://postgres:postgres@api_db_1:5432/postgres'
@@ -87,8 +87,32 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
+@app.route('/manga', methods=['POST', 'GET'])
+def add_maga():
+    filename = request.args.get('filename', None)
+    
+    if request.method == 'POST' and not filename:
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return '', 200
+    elif request.method == 'GET' and filename:
+        for file in os.listdir(UPLOAD_FOLDER):
+            if filename == file:
+                return send_file(os.path.join(UPLOAD_FOLDER, file), mimetype='image/gif')
+    else:
+        return '', 405
+
+
+@app.route('/kanji', methods=['GET', 'POST'])
+def add_kanji():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -103,7 +127,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('upload_file', name=filename))
+            return redirect(url_for('add_kanji', name=filename))
     return '''
     <!doctype html>
     <title>Upload new File</title>
